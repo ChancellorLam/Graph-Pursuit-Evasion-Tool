f66fb992d9916e8b18e3f5c3d1058c2e676da62f
ChancellorLam <135484498+ChancellorLam@users.noreply.github.com>
2024-06-11 14:39:26 -0400

    Added support for multiple probes

--- Graph-Pursuit-Evasion-Tool/main.py
+++ Graph-Pursuit-Evasion-Tool/main.py
@@ -2,7 +2,13 @@ from graph import create_franklin_graph
 
 if __name__ == '__main__':
     franklin_graph = create_franklin_graph()
-    print("What node would you like to probe?")
-    user_input = input().upper()
-    distances = franklin_graph.get_distances_from_node(user_input)
-    print(distances)
+    print("How many nodes would you like to probe?")
+    num_probes = int(input())
+
+    probe_locations = []
+    for i in range(0, num_probes):
+        print("What node would you like to probe?")
+        user_input = probe_locations.append(input().upper())
+    for probe in probe_locations:
+        distances = franklin_graph.get_distances_from_node(probe)
+        print(distances)
