d2f77ab729512e6361c078d39b5f1abf3cddbf53
ChancellorLam <135484498+ChancellorLam@users.noreply.github.com>
2024-06-11 10:06:12 -0400

    Added create_franklin_graph function

--- graph.py
+++ graph.py
@@ -36,3 +36,34 @@ class Graph:
     def add_node(self, node):
         self.nodes.append(node)
 
+
+def create_franklin_graph():
+    franklin_graph = Graph()
+
+    a = Node("A", ["B", "C", "E"])
+    b = Node("B", ["A", "D", "H"])
+    c = Node("C", ["A", "F", "J"])
+    d = Node("D", ["B", "G", "I"])
+    e = Node("E", ["A", "F", "K"])
+    f = Node("F", ["C", "E", "G"])
+    g = Node("G", ["D", "F", "H"])
+    h = Node("H", ["B", "G", "L"])
+    i = Node("I", ["D", "J", "K"])
+    j = Node("J", ["C", "I", "L"])
+    k = Node("K", ["E", "I", "L"])
+    l = Node("L", ["H", "J", "K"])
+
+    franklin_graph.add_node(a)
+    franklin_graph.add_node(b)
+    franklin_graph.add_node(c)
+    franklin_graph.add_node(d)
+    franklin_graph.add_node(e)
+    franklin_graph.add_node(f)
+    franklin_graph.add_node(g)
+    franklin_graph.add_node(h)
+    franklin_graph.add_node(i)
+    franklin_graph.add_node(j)
+    franklin_graph.add_node(k)
+    franklin_graph.add_node(l)
+
+    return franklin_graph
