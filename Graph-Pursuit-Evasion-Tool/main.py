e08198ea60f9fcc770b30f188d5e78c71f627270
ChancellorLam <135484498+ChancellorLam@users.noreply.github.com>
2024-06-12 14:45:03 -0400

    Added support for double star snark

--- Graph-Pursuit-Evasion-Tool/main.py
+++ Graph-Pursuit-Evasion-Tool/main.py
@@ -1,9 +1,11 @@
 from graph import create_franklin_graph
 from graph import create_loupekine_snark
+from graph import create_double_star_snark
 
 if __name__ == '__main__':
     franklin_graph = create_franklin_graph()
     loupekine_snark = create_loupekine_snark()
+    double_star_snark = create_double_star_snark()
     print("How many nodes would you like to probe?")
     num_probes = int(input())
 
@@ -14,7 +16,7 @@ if __name__ == '__main__':
     location_list = []
     for each_probe in probe_locations:
         # print(distances)
-        distances = loupekine_snark.get_distances_from_node(each_probe)
+        distances = double_star_snark.get_distances_from_node(each_probe)
         location_list.append(distances)
 
     result = {}
