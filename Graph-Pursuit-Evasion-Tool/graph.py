597b01bcfc85b8f64081611abe9813690c10fa01
ChancellorLam <135484498+ChancellorLam@users.noreply.github.com>
2024-06-26 10:39:35 -0400

    Added functionality to find metric dimension as well as support for second blanusa snark

--- Graph-Pursuit-Evasion-Tool/graph.py
+++ Graph-Pursuit-Evasion-Tool/graph.py
@@ -1,3 +1,5 @@
+from itertools import combinations
+
 from node import Node
 from collections import deque
 
@@ -28,6 +30,22 @@ class Graph:
 
         return distances
 
+    def compute_distance_matrix(self):
+        distance_matrix = {}
+        for node in self.nodes:
+            distance_matrix[node.name] = self.get_distances_from_node(node.name)
+        return distance_matrix
+
+    def get_metric_dimension(self):
+        distance_matrix = self.compute_distance_matrix()
+
+        for r in range(1, len(self.nodes) + 1):
+            for candidate_set in combinations(self.nodes, r):
+                if is_resolving_set(self, candidate_set, distance_matrix):
+                    return r, [node.name for node in candidate_set]
+
+        return None
+
     def probe_node(self, node_name):
         for node in self.nodes:
             if node_name == node.name:
@@ -35,6 +53,16 @@ class Graph:
 
     def add_node(self, node):
         self.nodes.append(node)
+
+
+def is_resolving_set(graph, candidate_set, distance_matrix):
+    all_distances = []
+
+    for node in graph.nodes:
+        distances = tuple(distance_matrix[node.name][cand.name] for cand in candidate_set)
+        all_distances.append(distances)
+
+    return len(all_distances) == len(set(all_distances))
 
 
 def create_franklin_graph():
@@ -187,3 +215,155 @@ def create_double_star_snark():
     double_star_snark.add_node(node30)
 
     return double_star_snark
+
+
+def create_szekeres_snark():
+    szekeres_snark = Graph()
+
+    node1 = Node('A', ['B', 'E', 'C'])
+    node2 = Node('B', ['D', 'O', 'A'])
+    node3 = Node('C', ['A', 'N', 'F'])
+    node4 = Node('D', ['KK', 'G', 'B'])
+    node5 = Node('E', ['A', 'G', 'H'])
+    node6 = Node('F', ['C', 'H', 'LL'])
+    node7 = Node('G', ['D', 'E', 'N'])
+    node8 = Node('H', ['F', 'E', 'O'])
+    node9 = Node('I', ['K', 'M', 'J'])
+    node10 = Node('J', ['I', 'P', 'L'])
+    node11 = Node('K', ['Q', 'Y', 'I'])
+    node12 = Node('L', ['J', 'Z', 'R'])
+    node13 = Node('M', ['I', 'S', 'U'])
+    node14 = Node('N', ['G', 'C', 'Z'])
+    node15 = Node('O', ['B', 'H', 'Y'])
+    node16 = Node('P', ['V', 'J', 'T'])
+    node17 = Node('Q', ['W', 'K', 'S'])
+    node18 = Node('R', ['L', 'T', 'X'])
+    node19 = Node('S', ['P', 'BB', 'AA'])
+    node20 = Node('T', ['R', 'CC', 'DD'])
+    node21 = Node('U', ['M', 'W', 'II'])
+    node22 = Node('V', ['P', 'X', 'JJ'])
+    node23 = Node('W', ['Q', 'AA', 'U'])
+    node24 = Node('X', ['R', 'DD', 'VV'])
+    node25 = Node('Y', ['K', 'BB', 'O'])
+    node26 = Node('Z', ['N', 'L', 'CC'])
+    node27 = Node('AA', ['W', 'BB', 'TT'])
+    node28 = Node('BB', ['AA', 'S', 'Y'])
+    node29 = Node('CC', ['Z', 'T', 'DD'])
+    node30 = Node('DD', ['CC', 'X', 'SS'])
+    node31 = Node('EE', ['KK', 'FF', 'MM'])
+    node32 = Node('FF', ['EE', 'GG', 'WW'])
+    node33 = Node('GG', ['FF', 'HH', 'XX'])
+    node34 = Node('HH', ['GG', 'LL', 'NN'])
+    node35 = Node('II', ['U', 'QQ', 'OO'])
+    node36 = Node('JJ', ['V', 'RR', 'PP'])
+    node37 = Node('KK', ['D', 'EE', 'QQ'])
+    node38 = Node('LL', ['F', 'HH', 'RR'])
+    node39 = Node('MM', ['EE', 'UU', 'OO'])
+    node40 = Node('NN', ['PP', 'HH', 'VV'])
+    node41 = Node('OO', ['II', 'MM', 'SS'])
+    node42 = Node('PP', ['JJ', 'NN', 'TT'])
+    node43 = Node('QQ', ['KK', 'II', 'UU'])
+    node44 = Node('RR', ['JJ', 'LL', 'VV'])
+    node45 = Node('SS', ['OO', 'DD', 'WW'])
+    node46 = Node('TT', ['PP', 'AA', 'XX'])
+    node47 = Node('UU', ['QQ', 'MM', 'WW'])
+    node48 = Node('VV', ['NN', 'XX', 'RR'])
+    node49 = Node('WW', ['FF', 'UU', 'SS'])
+    node50 = Node('XX', ['TT', 'GG', 'VV'])
+
+    szekeres_snark.add_node(node1)
+    szekeres_snark.add_node(node2)
+    szekeres_snark.add_node(node3)
+    szekeres_snark.add_node(node4)
+    szekeres_snark.add_node(node5)
+    szekeres_snark.add_node(node6)
+    szekeres_snark.add_node(node7)
+    szekeres_snark.add_node(node8)
+    szekeres_snark.add_node(node9)
+    szekeres_snark.add_node(node10)
+    szekeres_snark.add_node(node11)
+    szekeres_snark.add_node(node12)
+    szekeres_snark.add_node(node13)
+    szekeres_snark.add_node(node14)
+    szekeres_snark.add_node(node15)
+    szekeres_snark.add_node(node16)
+    szekeres_snark.add_node(node17)
+    szekeres_snark.add_node(node18)
+    szekeres_snark.add_node(node19)
+    szekeres_snark.add_node(node20)
+    szekeres_snark.add_node(node21)
+    szekeres_snark.add_node(node22)
+    szekeres_snark.add_node(node23)
+    szekeres_snark.add_node(node24)
+    szekeres_snark.add_node(node25)
+    szekeres_snark.add_node(node26)
+    szekeres_snark.add_node(node27)
+    szekeres_snark.add_node(node28)
+    szekeres_snark.add_node(node29)
+    szekeres_snark.add_node(node30)
+    szekeres_snark.add_node(node31)
+    szekeres_snark.add_node(node32)
+    szekeres_snark.add_node(node33)
+    szekeres_snark.add_node(node34)
+    szekeres_snark.add_node(node35)
+    szekeres_snark.add_node(node36)
+    szekeres_snark.add_node(node37)
+    szekeres_snark.add_node(node38)
+    szekeres_snark.add_node(node39)
+    szekeres_snark.add_node(node40)
+    szekeres_snark.add_node(node41)
+    szekeres_snark.add_node(node42)
+    szekeres_snark.add_node(node43)
+    szekeres_snark.add_node(node44)
+    szekeres_snark.add_node(node45)
+    szekeres_snark.add_node(node46)
+    szekeres_snark.add_node(node47)
+    szekeres_snark.add_node(node48)
+    szekeres_snark.add_node(node49)
+    szekeres_snark.add_node(node50)
+
+    return szekeres_snark
+
+
+def create_second_blanusa_snark():
+    second_blanusa_snark = Graph()
+
+    node1 = Node("A", ["B", "C", "E"])
+    node2 = Node("B", ["A", "D", "K"])
+    node3 = Node("C", ["A", "M", "N"])
+    node4 = Node("D", ["B", "P", "Q"])
+    node5 = Node("E", ["A", "F", "L"])
+    node6 = Node("F", ["E", "G", "N"])
+    node7 = Node("G", ["F", "H", "M"])
+    node8 = Node("H", ["G", "I", "O"])
+    node9 = Node("I", ["H", "J", "Q"])
+    node10 = Node("J", ["I", "K", "P"])
+    node11 = Node("K", ["B", "J", "R"])
+    node12 = Node("L", ["E", "M", "R"])
+    node13 = Node("M", ["C", "G", "L"])
+    node14 = Node("N", ["C", "F", "O"])
+    node15 = Node("O", ["H", "N", "P"])
+    node16 = Node("P", ["D", "J", "O"])
+    node17 = Node("Q", ["D", "I", "R"])
+    node18 = Node("R", ["K", "L", "Q"])
+
+    second_blanusa_snark.add_node(node1)
+    second_blanusa_snark.add_node(node2)
+    second_blanusa_snark.add_node(node3)
+    second_blanusa_snark.add_node(node4)
+    second_blanusa_snark.add_node(node5)
+    second_blanusa_snark.add_node(node6)
+    second_blanusa_snark.add_node(node7)
+    second_blanusa_snark.add_node(node8)
+    second_blanusa_snark.add_node(node9)
+    second_blanusa_snark.add_node(node10)
+    second_blanusa_snark.add_node(node11)
+    second_blanusa_snark.add_node(node12)
+    second_blanusa_snark.add_node(node13)
+    second_blanusa_snark.add_node(node14)
+    second_blanusa_snark.add_node(node15)
+    second_blanusa_snark.add_node(node16)
+    second_blanusa_snark.add_node(node17)
+    second_blanusa_snark.add_node(node18)
+
+    return second_blanusa_snark
--- Graph-Pursuit-Evasion-Tool/main.py
+++ Graph-Pursuit-Evasion-Tool/main.py
@@ -1,11 +1,26 @@
-from graph import create_franklin_graph
-from graph import create_loupekine_snark
-from graph import create_double_star_snark
+from graph import (create_franklin_graph, create_loupekine_snark, create_double_star_snark, create_szekeres_snark,
+                   create_second_blanusa_snark)
 
 if __name__ == '__main__':
     franklin_graph = create_franklin_graph()
     loupekine_snark = create_loupekine_snark()
     double_star_snark = create_double_star_snark()
+    szekeres_snark = create_szekeres_snark()
+    second_blanusa_snark = create_second_blanusa_snark()
+
+    print("Metric Dimension: " + str(second_blanusa_snark.get_metric_dimension()))
+
+    # # generate every list of every combination
+    # graph_names = []
+    # for node in franklin_graph.nodes:
+    #     graph_names.append(node.name)
+    #
+    # test = []
+    # for i in range(1, len(graph_names) + 1):
+    #     els = [list(x) for x in combinations(graph_names, i)]
+    #     test.extend(els)
+    # print(test)
+
     print("How many nodes would you like to probe?")
     num_probes = int(input())
 
@@ -16,7 +31,7 @@ if __name__ == '__main__':
     location_list = []
     for each_probe in probe_locations:
         # print(distances)
-        distances = double_star_snark.get_distances_from_node(each_probe)
+        distances = second_blanusa_snark.get_distances_from_node(each_probe)
         location_list.append(distances)
 
     result = {}
